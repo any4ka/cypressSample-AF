09f99d1984a33683619ff012b774209cff77d582
Anna Finch <anna.k.finch@gmail.com>
2023-06-03 16:52:49 -0500

    add UI tests

--- cypress/e2e/sampleUITest.cy.js
+++ cypress/e2e/sampleUITest.cy.js
@@ -1,20 +1,36 @@
 describe('template spec', () => {
   it('querying commands on cypress.io example site', () => {
+    // GO to cypress test page
     cy.visit('https://example.cypress.io')
+    cy.get('a').contains('Querying').click({ force: true });
+
+    //Click around the Quering page, and assert the text
+    cy.get('#query-btn').should('contain', 'Button')
+    cy.get('.query-btn').should('contain', 'Button')
+    cy.get('#querying .well>button:first').should('contain', 'Button')
+
+    cy.go("back")
+    cy.get('a').contains('Actions').click({ force: true });
+    cy.get('.action-form')
+      .find('[type="text"]').scrollIntoView().type('HALFOFF')
+    cy.get('.action-form').submit()
+      .next().should('contain', 'Your form has been submitted!')
   })
-  it('Traversal commands on cypress.io example site',()=>{
-    cy.visit('https://example.cypress.io')
-  })
-  it('Actions commands on cypress.io example site',()=>{
-    cy.visit('https://example.cypress.io')
-  })
-  it('Winow and Viewport commands on cypress.io example site',()=>{
-    cy.visit('https://example.cypress.io')
-  })
-  it('Location commands on cypress.io example site',()=>{
-    cy.visit('https://example.cypress.io')
-  })
-  it('Navigation commands on cypress.io example site',()=>{
-    cy.visit('https://example.cypress.io')
+
+  it('mock data for an angular page', () => {
+    cy.intercept('GET', '**/tags', { fixture: 'tags.json' }).as('tags')
+    cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
+      .as('articles')
+    cy.visit('https://angular.realworld.io/')
+
+    //check tags
+    cy.get('.tag-list', { timeout: 10000 })
+      .should('contain', 'cypress')
+      .and('contain', 'selenium')
+
+    //check articles
+    cy.get('app-favorite-button.pull-xs-right').contains('10')
+    cy.get('.author').contains('testersdock')
+    cy.get('.preview-link > p').contains('This is a test description')
   })
 })
